c8cd0f70d384879287a03cf3371e336b341e9da0
tony9402 <tony9402@naver.com>
2026-05-31 20:34:21 +0900

    Generator 수정

--- problems/23/validator/validator.cpp
+++ problems/23/validator/validator.cpp
@@ -3,12 +3,12 @@
 int main(int argc, char* argv[]) {
     registerValidation(argc, argv);
     
-    int N = inf.readInt(1, 30'000); inf.readSpace();
+    int N = inf.readInt(1, 10'000); inf.readSpace();
     int M = inf.readInt(1, 300'000'000);
     inf.readEoln();
     
     for(int i = 0; i < N; ++i) {
-        int x = inf.readInt(1, 10'000);
+        int x = inf.readInt(1, 30'000);
         if(i + 1 == N) inf.readEoln();
         else inf.readSpace();
     }
